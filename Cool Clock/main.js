261dd102bfdbb92b6991dc342f97250c5c075e0c
AxelAlinsky <66462743+AxelAlinsky@users.noreply.github.com>
2022-02-14 22:52:36 +0700

    Cool Clock

---
+++ Cool Clock/main.js
@@ -0,0 +1,30 @@
+function displayTime() {
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById("session");
+
+    if (hrs > 12) {
+        hrs = hrs - 12;
+        session.innerHTML = "PM";
+    }
+    if (hrs < 10) {
+        hrs = "0" + hrs;
+    }
+
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    
+    if (min < 10) {
+        min = "0" + min;
+    }
+
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+
+setInterval(displayTime, 10)
